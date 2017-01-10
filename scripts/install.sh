b8f10ea07ed43c8c15292a8d220f2b54d6283fb5
Benjamin Caldwell <benjamincaldwell@users.noreply.github.com>
2017-01-10 08:36:07 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,7 +21,7 @@ set -e
 
   LATEST_DFM_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/benjamincaldwell/dfm/releases/latest | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["tag_name"]')
 
-  wget "https://github.com/benjamincaldwell/dfm/releases/download/${LATEST_DFM_VERSION}/dfm_$(_dfm_system_detector)" -O /tmp/dfm
+  curl "https://github.com/benjamincaldwell/dfm/releases/download/${LATEST_DFM_VERSION}/dfm_$(_dfm_system_detector)" -O /tmp/dfm
   chmod +x /tmp/dfm
   echo "Moving binary to /usr/local/bin/"
   sudo mv /tmp/dfm /usr/local/bin/
